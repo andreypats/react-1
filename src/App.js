c6cd4b98d6a29d2171bccc3fd7898d1df677f2b0
andreypats <andrey.it.work@gmail.com>
2022-07-21 15:27:57 +0300

    Delete Header.js, Technologies.js and change App.css, App.css

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from "react";
 import './App.css';
-import Technologies from './Technologies.js';
-import Header from './Header.js';
 
 const App = () => { // компонента - функция, возвращающая разметку JSX (HTML внутри JS)
-    //новый комментарий - проверяем git
     return (
-        <div>
-            <Header />
-            <Technologies />
+        <div className={'app-wrapper'}>
+            <header className={'header'}>
+                <img
+                    alt={""}
+                    src={'https://avatars.mds.yandex.net/i?id=afa7816f8b9e4bfaa5b33c023d3f954b-4571459-images-thumbs&n=13'}/>
+            </header>
+            <nav className={'nav'}>
+                <div>Profile</div>
+                <div>Messages</div>
+            </nav>
+            <div className={'content'}>
+                Main content
+            </div>
+
+
         </div>
     );
 }
